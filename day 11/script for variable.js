94a8209e96bb537e19e7eca75f6cf5c2ffc8c40a
Ankit82mohanty <ankitmohanty8260@gmail.com>
2025-07-24 08:31:35 +0530

    css &  javascript files

---
+++ day 11/script for variable.js
@@ -0,0 +1,77 @@
+// let :-
+
+let username = 'Ankit'
+console.log(username)
+console.log(typeof username)
+
+let age = 21
+console.log(age)
+console.log(typeof age)
+
+let isHappy= true
+console.log(isHappy)
+console.log(typeof isHappy)
+
+let lastName = 'mohanty'
+console.log(lastName)
+console.log(typeof lastName)
+
+
+
+
+
+
+let userIntro = "hii i am "+ username +" "+ lastName +", i am "+ age + "years old"
+console.log(userIntro)
+
+let a = undefined
+console.log(a)
+console.log(typeof a)
+
+let b;
+console.log(b)
+console.log(typeof b)
+
+b = false
+console.log(b)
+console.log(b,typeof b, 'xyz')
+
+let c = null
+console.log(c, typeof c)
+
+// <===========================>
+
+ // const :-
+
+ const housrsInADay = 24
+//  housrsInADay = 20
+// housrsInADay = "jt"
+
+// const d;
+// <==============================>
+
+// var :-
+
+var e
+ e= 10 
+ console.log(e)
+
+ var f
+console.log(f)
+
+// var g = 120
+
+// {
+//     var g = 20000
+//     let h = 30; 
+// }
+// console.log(g)
+// console.log(h)
+
+// console.log(x) error- before declaration we cannot access the variable
+let x = 2020;
+console.log(x)
+
+console.log( 'before initialitasation the value of y', y) //undifined
+var y= 3020
+console.log(y)
---
+++ day 12/script or debugger.js
@@ -0,0 +1,13 @@
+// debugger
+
+// console.log(firstName)
+// console.log(lastName)
+// console.log(age)
+
+var firstName ="Ankit"
+let lastName ="Mohanty"
+const age = 21
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
