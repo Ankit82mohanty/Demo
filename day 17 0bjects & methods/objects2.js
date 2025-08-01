b3cecd072b36a0a5be400c1a679a6bbf6ac03b95
Ankit82mohanty <ankitmohanty8260@gmail.com>
2025-08-01 07:20:00 +0530

    OBJECTS and METHODS in JS

--- day 17 0bjects & methods/objects2.js
+++ day 17 0bjects & methods/objects2.js
@@ -16,3 +16,45 @@ const person = {
 //console.log(person.lastname)
 
 //person = {} //error -> as person is declared with const
+
+// let = person2 ={
+//     firstname : 'Raj',
+//     lastname : 'patra'
+// }
+//  person2 = {} //empty object
+
+
+//DELETE :-
+//  const isDeleted = delete person.lastname
+//  console.log(isDeleted)
+// console.log(person)
+
+// console result:-
+// --------------------
+// delete person.lastname
+// true
+// delete person.firstname
+// true
+// person
+// {}[[Prototype]]: Object
+// delete person.age
+// true
+
+//  Object.seal(person) // it restrict the object ,so that any addition & deletion of any key can't be possible (still updating key & value are possible)
+
+//  console results:-
+// ------------------------------
+//  person
+// {firstname: 'ankit', lastname: 'mohanty'}
+// delete person.firstname
+// false
+//  person.lastname = 'sahoo'
+// 'sahoo'
+// person
+// {firstname: 'ankit', lastname: 'sahoo'}
+// person.age =23
+// 23
+// person
+// {firstname: 'ankit', lastname: 'sahoo'}
+
+Object.freeze(person) // through freeze method, we can't make any kind of operation (expect extracting values)
