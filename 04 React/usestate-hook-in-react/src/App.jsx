818557c7ebf554e33b97044569131bc352c01a49
Ankit82mohanty <ankitmohanty8260@gmail.com>
2025-09-09 10:10:41 +0530

    changed usestate

--- 04 React/usestate-hook-in-react/src/App.jsx
+++ 04 React/usestate-hook-in-react/src/App.jsx
@@ -2,51 +2,79 @@ import { useState } from 'react';
 import './App.css'
 
 let count = 0;
-//const data= useState(0)           // useState(initialValue)
 
+// const data = useState(0) // [state, setState] = useState(initialValue)
 
-function App({root}) {
-  // const data = useState(0)
-  // console.log('data:-',data)
+console.log("Hellloooooo")
 
-  const [data,setData]= useState(0)
-
+function App({ root }) {
+  // const data = useState(100)
+  // console.log('data:-', data)
   
-  const IncreaseValue =(e)=>{
-     console.log("increase buttton clicked")
+  const [counter, setCounter] = useState(100)
+  console.log("rendering", counter)
+
+  const increaseValue = (e) => {
+    console.log("Increase button clicked")
     // count++
-    // console.log("count:-",count)
-    // root.render(<App root={root}/>)
+    // console.log('count:-', count)
+    // root.render(<App root={root} />)
 
     // data[0]++
-  
-
+    // console.log(data[0])
+    
     // const setData = data[1]
-    // console.log(setData)
+    // // console.log(setData)
     // setData(data[0]++)
     // console.log(data[0])
 
-    setData(data+1)
+    // setCounter(counter + 1)
+    // setCounter(100)
+    // console.log("Counter Value :-", counter)
 
+    // console.log("Counter Value ", counter)
+    // setCounter(counter + 1) // counter = 100, setCounter = 100 + 1 => 101
+    // console.log("Counter Value ", counter)
+    // setCounter(counter + 1) // counter = 100, setCounter = 100 + 1 => 101
+    // console.log("Counter Value ", counter)
+    // setCounter(counter + 1) // counter = 100, setCounter = 100 + 1 => 101 
+    // console.log("Counter Value ", counter)
+
+    // setCounter((prevState) => {
+    //   console.log("1st Previous State is ", prevState)
+    //   console.log("1st Counter Value ", counter)
+    //   return prevState + 1; 
+    // })
+    // setCounter((prevState) => {
+    //   console.log("2nd Previous State is ", prevState) 
+    //   console.log("2nd Counter Value ", counter)
+    //   return prevState + 1; 
+    // })
+    // setCounter((prevState) => {
+    //   console.log("3rd Previous State is ", prevState) 
+    //   console.log("3rd Counter Value ", counter)
+    //   return prevState + 1; 
+    // })
+
+    setCounter(prevState => prevState + 1)
+    setCounter(prevState => prevState + 1)
+    setCounter(prevState => prevState + 1)
   }
-  const DecreaseValue =(e)=>{
-    console.log("decrease button clicked")
-    setData(data-1)
-    
 
+  const decreaseValue = (e) => {
+    console.log("Decrease button clicked")
   }
-
-  
 
   return (
     <>
       <h1>Counter App</h1>
-      <button onClick={IncreaseValue}>Increase</button>
+
+      <button onClick={increaseValue}>Increase</button>
       <br /> <br />
-      <button>{data}</button>
-      <br /> <br />
-      <button onClick={DecreaseValue}>Decrease</button>
-     </>
+      <button>{counter}</button>
+      <br />  <br />
+      <button onClick={decreaseValue}>Decrease</button>
+    </>
   )
 }
 
